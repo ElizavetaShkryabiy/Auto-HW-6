861f4a83c215d18421ebf134f7f56c29f63c40e5
Elizaveta Shkryabiy <evshkryabiy@gmail.com>
2021-10-28 20:11:00 +0400

    Tests for possible bugs added

--- src/test/java/ru/netology/web/page/TransferPage.java
+++ src/test/java/ru/netology/web/page/TransferPage.java
@@ -1,6 +1,7 @@
 package ru.netology.web.page;
 
 import com.codeborne.selenide.SelenideElement;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
@@ -9,17 +10,27 @@ import static com.codeborne.selenide.Selenide.$$;
 public class TransferPage {
     private SelenideElement heading = $$(".heading").find(text("Пополнение карты"));
     private SelenideElement sum = $("[data-test-id=amount] input");
+
+    public SelenideElement getSum() {
+        return sum;
+    }
+
+
     private SelenideElement from = $("[data-test-id=from] input");
     private SelenideElement transfer = $("[data-test-id=action-transfer]");
 
     public TransferPage() {
         heading.shouldBe(visible);
     }
-    public DashboardPage successfulTransfer(int amount, String cardId) {
+    public DashboardPage successfulTransfer(int neededCardBalance, int amount, String cardId) {
+
         sum.setValue(String.valueOf(amount));
         from.setValue(cardId);
+        if (amount<neededCardBalance){
         transfer.click();
-        return new DashboardPage();
+            return new DashboardPage();
+        }else{return null;}
+
     }
 
 }
--- src/test/java/ru/netology/web/test/ErrorNotificationTest.java
+++ src/test/java/ru/netology/web/test/ErrorNotificationTest.java
@@ -1,14 +1,11 @@
 package ru.netology.web.test;
 
-
 import com.github.javafaker.Faker;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.DashboardPage;
 import ru.netology.web.page.LoginPage;
-import ru.netology.web.page.TransferPage;
 
 import java.time.Duration;
 import java.util.Locale;
@@ -18,7 +15,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-class MoneyTransferTest {
+public class ErrorNotificationTest {
     DataHelper.Card card1 = DataHelper.getFirstCardInfo();
     DataHelper.Card card2 = DataHelper.getSecondCardInfo();
 
@@ -34,23 +31,34 @@ class MoneyTransferTest {
         verificationPage.validVerify(verificationCode);
     }
 
-    @AfterEach
-    void shouldCheckBalance() {
+    @Test
+    void shouldNotTransferWithInvalidCardNumber() {
+        final Faker faker = new Faker(Locale.forLanguageTag("ru"));
+        String cardNumber = faker.business().creditCardNumber();
         var dashboardPage = new DashboardPage();
-        dashboardPage.getCardBalance(card1.getVisiblePart());
-        dashboardPage.getCardBalance(card2.getVisiblePart());
+        var neededCardBalance = 1000000000;
+        int amount = 1000;
+        dashboardPage.transferMoney(card2.getDepositButton()).successfulTransfer(neededCardBalance, amount, cardNumber);
+        $("[data-test-id=error-notification]").shouldBe(appear, Duration.ofSeconds(7))
+                .shouldHave(text("Ошибка! Произошла ошибка"));
     }
 
     @Test
-    void shouldTransferMoneyBetweenOwnCards() {
+    void shouldNotTransferMoreThanThereIsOnCard() {
         var dashboardPage = new DashboardPage();
         var neededCardBalance = dashboardPage.getCardBalance(card1.getVisiblePart());
-        int amount = 100;
+        int amount = 10500;
         dashboardPage.transferMoney(card2.getDepositButton()).successfulTransfer(neededCardBalance, amount, card1.getNumber());
+        $("[data-test-id=error-notification]").shouldBe(appear, Duration.ofSeconds(7));
     }
 
+    @Test
+    void shouldNotTransferWithMinusAmount() {
+        var dashboardPage = new DashboardPage();
+        var neededCardBalance = dashboardPage.getCardBalance(card1.getVisiblePart());
+        int amount = -1000;
+        dashboardPage.transferMoney(card2.getDepositButton()).successfulTransfer(neededCardBalance, amount, card1.getNumber());
+        $("[data-test-id=error-notification]").shouldBe(appear, Duration.ofSeconds(7));
 
-
-
+    }
 }
-
